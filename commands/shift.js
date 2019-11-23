5cca6d359ec7b57bdcd5c73799c63d6b88173537
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-11-23 18:49:50 -0500

    Add files via upload

---
+++ commands/shift.js
@@ -0,0 +1,62 @@
+
+const Discord = require("discord.js");
+
+const botconfig = require("../botconfig.json");
+
+const red = botconfig.red;
+
+const green = botconfig.green;
+
+const orange = botconfig.orange;
+
+const errors = require("../utils/errors.js");
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+    message.delete();
+
+    if(args[0] == "help"){
+
+      message.reply("Usage: !report <user> <reason>");
+
+      return;
+
+    }
+
+
+
+    let reportEmbed = new Discord.RichEmbed()
+
+    .setTitle("A SHIFT HAS BEEN SCHEDULED!")
+
+    .setColor()
+
+    .setDescription("There is a shift scheduled, and we'd like you to join us! Another announcement will be made when the shift has commenced;")
+
+    .addField("**SCHEDULED AT**", message.createdAt)
+
+    .addField("**HOST**", message.author)
+
+    .addField("**STATUS**", "Open");
+
+
+
+    let reportschannel = message.guild.channels.find(`name`, "session-notifications");
+
+    if(!reportschannel) return message.channel.send("Couldn't find sessions channel.");
+
+    reportschannel.send(reportEmbed);
+
+
+
+}
+
+
+
+module.exports.help = {
+
+  name: "shift"
+
+}
