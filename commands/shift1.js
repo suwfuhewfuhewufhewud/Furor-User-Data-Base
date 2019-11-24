0f1ca6d0826659710dc4403880515ff324e4fb66
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-11-24 15:14:24 -0500

    Update shift1.js

--- commands/shift1.js
+++ commands/shift1.js
@@ -1,40 +1,20 @@
 
 const Discord = require("discord.js");
-3
-​
-4
+
 const botconfig = require("../botconfig.json");
-5
-​
-6
+
 const red = botconfig.red;
-7
-​
-8
+
 const green = botconfig.green;
-9
-​
-10
+
 const orange = botconfig.orange;
-11
-​
-12
+
 const errors = require("../utils/errors.js");
-13
-​
-14
-​
-15
-​
-16
+
 module.exports.run = async (bot, message, args) => {
-17
-​
-18
+
     message.delete();
-19
-​
-20
+
     if(args[0] == "help"){
 
       message.reply("Usage: !report <user> <reason>");
