79e3df3b58be40dd0d973b6ce309a9840f129fc5
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-11-24 15:29:52 -0500

    Update alliance.js

--- commands/alliance.js
+++ commands/alliance.js
@@ -1,59 +1,57 @@
-const { RichEmbed } = require("discord.js");
+const Discord = require("discord.js");
 
+const botconfig = require("../botconfig.json");
 
+const red = botconfig.red;
 
-module.exports = {
+const green = botconfig.green;
 
-    name: "report",
+const orange = botconfig.orange;
 
-    category: "moderation",
+const errors = require("../utils/errors.js");
 
-    description: "Reports a member",
+    let username = args.slice(1).join("");
+    let uid = message.author.id;
 
-    usage: "<mention, id>",
-
-    run: async (client, message, args) => {
-
-        if (message.deletable) message.delete();
-
-        if (!args[1])
-
-            return message.channel.send("Please provide a reason for the report").then(m => m.delete(5000));
-
-        
-
-        const channel = message.guild.channels.find(c => c.name === "ally-announcements")
-
-            
-
-        if (!channel)
-
-            return message.channel.send("Couldn't find a `#reports` channel").then(m => m.delete(5000));
-
-
-
-        const embed = new RichEmbed()
-
-            .setColor("#ff0000")
-
-            .setTitle("NEW ALLIANCE")
-
-            .setTimestamp()
-
-            .setFooter(message.guild.name, message.guild.iconURL)
-
-            .addField("**Group Link**", `${args.slice(1).join(" ")}`);
-
-
-
-        return channel.send(embed);
-
-    }
-
-}
+    message.channel.send("Working, please wait.").then(msg => {
+      // make request to api and get link code
+      let url = "https://verify.eryn.io/api/user/${this.id}";
+      request(url, (err, resp, body) => {
+        if (!err && (resp.statusCode == 302 || resp.statusCode == 200)) {
+          let j = JSON.parse(body);
+          if (!j["ok"])
+            return msg.edit("Did you pass your username as an argument? e.g a!link USERNAME");
+          if (j["isLinked"]) {
+            msg.edit("Your Discord account is already linked to AppX!");
+          } else {
+            msg.edit(
+              "I am going to DM you a list of instructions on how to link your account!"
+            );
+            message.author
+              .createDM()
+              .then(dm => {
+                dm.send(
+                  `In order to link your account please go to the **AppX Dashboard** then click on **My Account -> Account Linking**. Enter the following code in the *Link Code* box: \`${
+                    j["linkCode"]
+                  }\` Then press *Link*!`
+                );
+              })
+              .catch(() => {
+                msg.edit(
+                  "I can't DM you! Have you blocked me or closed/disabled your DMs?"
+                );
+              });
+          }
+        } else {
+          msg.edit("Unable to make request to AppX API!");
+          console.log(resp.statusCode);
+        }
+      });
+    });
+  }
 
 module.exports.help = {
 
-    name: "commenced"
-  
-  }
+  name: "link"
+
+}
