58877415d28368449757749646251a6cc56cc5b1
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-11-24 15:13:02 -0500

    Update shift1.js

--- commands/shift1.js
+++ commands/shift1.js
@@ -1,22 +1,40 @@
 
 const Discord = require("discord.js");
-
+3
+​
+4
 const botconfig = require("../botconfig.json");
-
+5
+​
+6
 const red = botconfig.red;
-
+7
+​
+8
 const green = botconfig.green;
-
+9
+​
+10
 const orange = botconfig.orange;
-
+11
+​
+12
 const errors = require("../utils/errors.js");
-
-
-
+13
+​
+14
+​
+15
+​
+16
 module.exports.run = async (bot, message, args) => {
-
+17
+​
+18
     message.delete();
-
+19
+​
+20
     if(args[0] == "help"){
 
       message.reply("Usage: !report <user> <reason>");
@@ -25,27 +43,20 @@ module.exports.run = async (bot, message, args) => {
 
     }
 
-
-
     let reportEmbed = new Discord.RichEmbed()
 
-    .setTitle("ERROR")
+    .setTitle("A SHIFT HAS NOW COMMENCED!")
 
     .setColor()
+    .setDescription("There is a shift whitch recently commanced! Feel free to join the cafe with some of your friends and enjoy a refreshing Boba Tea with the rest of our community!")
 
-    .setDescription("AN Error has occured! Please contact JordanStrunk!")
+    .addField("**HOST**", message.author)
 
+    .addField("**STATUS**", "Open");
 
      message.channel.send(reportEmbed);
-
-
-
-}
-
-
 
 module.exports.help = {
 
   name: "commenced"
-
 }
