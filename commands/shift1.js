bb8ee2b30ec812605b4f6e0a0f2d1a10039fd2f4
AbovePlanet302 <35471112+AbovePlanet302@users.noreply.github.com>
2019-11-24 21:34:10 -0500

    Update shift1.js

--- commands/shift1.js
+++ commands/shift1.js
@@ -25,14 +25,10 @@ module.exports.run = async (bot, message, args) => {
 
     let reportEmbed = new Discord.RichEmbed()
 
-    .setTitle("A SHIFT HAS NOW COMMENCED!")
+    .setTitle("ERROR")
 
     .setColor()
-    .setDescription("There is a shift whitch recently commanced! Feel free to join the cafe with some of your friends and enjoy a refreshing Boba Tea with the rest of our community!")
-
-    .addField("**HOST**", message.author)
-
-    .addField("**STATUS**", "Open");
+    .setDescription("Error! Please Contact JordanStrunk!")
 
      message.channel.send(reportEmbed);
 
