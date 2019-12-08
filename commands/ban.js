dca07ce029e512d694cdf98cdd8c647915dc74bc
suwfuhewfuhewufhewud <58646995+suwfuhewfuhewufhewud@users.noreply.github.com>
2019-12-07 23:08:15 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,33 +1,51 @@
 const Discord = require("discord.js");
+
+const botconfig = require("../botconfig.json");
+
+const red = botconfig.red;
+
+const green = botconfig.green;
+
+const orange = botconfig.orange;
+
 const errors = require("../utils/errors.js");
+
+
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("KICK_MEMBERS")) return errors.noPerms(message, "KICK_MEMBERS");
+    message.delete();
+
     if(args[0] == "help"){
-      message.reply("Usage: !kick <user> <reason>");
+
+      message.reply("Usage: !report <user> <reason>");
+
       return;
+
     }
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return errors.cantfindUser(message.channel);
-    let kReason = args.join(" ").slice(22);
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, kUser, "MANAGE_MESSAGES");
+    let rreason = args.join(" ").slice(0);
+    if(!rreason) return errors.noReason(message.channel);
 
-    let kickEmbed = new Discord.RichEmbed()
-    .setTitile("User Banned")
-    .setColor()
-    .addField("Banned User", `${kUser}`)
-    .addField("Banned By", `<@${message.author.id}>`)
-    .addField("Time", message.createdAt)
-    .addField("Reason", kReason);
 
-    let kickChannel = message.guild.channels.find(`name`, "kick-ban-logs");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
 
-    message.guild.member(kUser).ban(kReason);
-    kickChannel.send(kickEmbed);
+    let reportEmbed = new Discord.RichEmbed()
+
+    .setTitle("ADVERTISEMENT DECLINED")
+    
+    .setdescription("rreason")
+
+
+
+     message.channel.send(reportEmbed);
+
+
+
 }
 
+
+
 module.exports.help = {
-  name:"ban"
+
+  name: "decline"
+
 }
